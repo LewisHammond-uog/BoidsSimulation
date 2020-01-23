341b0b8c431f0897659bbb4ca7e3da6b3f06f065
LewisHammond-uog <s1800707@connect.glos.ac.uk>
2020-01-23 16:21:37 +0000

    Static Libary Imports
    Installed GLWF, GLAD, STB Image, GLM and Assimp

---
+++ ModelLoader/ModelLoader/main.cpp
@@ -0,0 +1,21 @@
+
+//OpenGL Headers
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+//GLM Headers
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+//STB Headers
+#include <stb/stb_image.h>
+
+//Assimp Headers
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+int main() {
+	return 0;
+}
