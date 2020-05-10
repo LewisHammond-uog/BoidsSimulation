ad62ebab9b599cc643adc7fb4c2f13dfb29f1885
Lewis Hammond <s1800707@connect.glos.ac.uk>
2020-05-11 00:38:52 +0100

    Rebuild (Debug and Release)
    - Rebuilt to avoid any caching errors
    - Added Description Comment in Main with refrences

--- ModelLoader/ModelLoader/source/main.cpp
+++ ModelLoader/ModelLoader/source/main.cpp
@@ -1,3 +1,21 @@
+/// <summary>
+/// Boids Application created for CT 5037 by Lewis Hammond (s1800707)
+/// Generates a number of boid like objects and applies flocking, steering and collision avoidance
+/// behavours to them.
+///
+/// Uses:
+/// ReactPhysics3D
+/// DearImgui
+/// GLAD
+/// GLFW
+/// OpenGL
+/// AssImp
+/// STB Image
+/// Gizmos.h
+///
+/// Fish Models from Turbo Squid: https://www.turbosquid.com/FullPreview/Index.cfm/ID/652729
+/// </summary>
+
 
 #include "Scene.h"
 
